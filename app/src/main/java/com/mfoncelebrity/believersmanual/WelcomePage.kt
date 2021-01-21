86b2010e1b2c0589f2bd5ba4f900e66ca0f7fa21
francisoni <powellgreen.pg@gmail.com>
2021-01-21 15:27:39 +0100

    project

--- app/src/main/java/com/mfoncelebrity/believersmanual/WelcomePage.kt
+++ app/src/main/java/com/mfoncelebrity/believersmanual/WelcomePage.kt
@@ -3,9 +3,14 @@ package com.mfoncelebrity.believersmanual
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class WelcomePage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_welcomepage)
+
+
+
     }
 }
+
+
